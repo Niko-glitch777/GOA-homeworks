018b72482e47be195b85c5657fe2181416d21c5a
Niko-glitch777 <nikatsomaia88@gmail.com>
2025-10-05 11:09:41 +0400

    “commit”

---
+++ day02/homework/hw.py
@@ -0,0 +1,31 @@
+
+
+name = ("Niko")
+print(name)
+
+lastname = ("Tsomaia")
+print(lastname) 
+
+
+number = 5          # <-- integer
+print(number)
+
+movie = ("dumb and dumber")    # <-- string
+print(movie)
+
+
+me=(12)
+mom=(30)
+dad=(32)
+auntdadssister=(30)
+uncledadsside=(34)
+grandmadadsside=(53)
+grandpadadsside=(54)
+auntdadsside=(31)
+cousindadsside=(14)
+auntmomsside=(32)
+grandmamomsside=(70)
+unclemomsside=(33)
+cousinmomssideana=(3)
+cousinmomssidegio=(11)
+print(me+mom+dad+auntdadssister+uncledadsside+grandmadadsside+grandpadadsside+auntdadsside+cousindadsside+grandmamomsside+auntmomsside+unclemomsside+cousinmomssideana+cousinmomssidegio)
