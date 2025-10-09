e74bca05993b8f13e44b42e7cf1d7d1e0fb3c66b
Niko-glitch777 <nikatsomaia88@gmail.com>
2025-10-10 00:15:30 +0400

    “commit”

---
+++ day04/classwork/cw.py
@@ -0,0 +1,30 @@
+#string - "string" არის ტექსტი მოქცეული ბრჭყალებში.
+
+#მაგალითი : Name=("Test")
+
+#float - "float" გამოიყენება სუყველა ათწილადი რიცხვისთვის.
+
+#მაგალითი : 2-1.5=0.5
+
+#integer - "integer" არის ნებისიმიერი სრული რიცხვი
+
+#მაგალითი : Age=(1)
+
+#boolean - "boolean" არის გამოყენებული True ან False ის დასადგენად
+
+#მაგალითი : School is real = True
+
+
+
+
+Name=("luka")
+Name=("gurami")
+Name=("Niko")
+print(Name)
+
+
+Gio=(15)
+Luka=(16)
+Both=(31)
+
+print(Both)
