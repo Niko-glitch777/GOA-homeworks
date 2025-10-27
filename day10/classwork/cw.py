9dba17154e6a0b389dc2f221b5a973985e548413
Niko-glitch777 <nikatsomaia88@gmail.com>
2025-10-27 23:35:48 +0400

    “commit”

---
+++ day10/classwork/cw.py
@@ -0,0 +1,45 @@
+# მკაცრი ოფერატორები არის:
+#==
+#>
+#!=
+#<
+
+
+# არამკაცრი ოპერატორები არის :
+#<=
+#>=
+
+password = 1212
+user_password=input('Enter your password: ')
+print(password == user_password)
+
+age=10
+user_age=int(input("Enter your age: "))
+print(age < user_age)
+
+
+num1=11
+num2= 9
+num3=6
+num4=4
+num5=5
+user_num=int(input('Enter your num: '))
+print(user_num < num1)
+print(user_num < num2)
+print(user_num < num3)
+print(user_num < num4)
+print(user_num < num5)
+
+
+
+num6=20
+num7=21
+num8=43
+num9=32
+num10=53
+user_num2=int(input('Enter your 2nd num: '))
+print(user_num < num6)
+print(user_num < num7)
+print(user_num < num8)
+print(user_num < num9)
+print(user_num < num10)
---
+++ day10/homework/hw.py
@@ -0,0 +1,7 @@
+animal_name = "Lion"
+animal_name1 = input("Enter your animal here: ")
+print(animal_name == animal_name1)
+
+my_age = 12
+friend_age = int(input("Enter bestfriend age here: "))
+print(my_age > friend_age)
