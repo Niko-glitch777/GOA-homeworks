2237e7f11f73acd509af8c439eab7746681773f0
Niko-glitch777 <nikatsomaia88@gmail.com>
2025-11-04 20:35:27 +0400

    “commit”

---
+++ day12/classwork/cw.py
@@ -0,0 +1,26 @@
+#num_1 = int(input('Enter first number: '))
+#num_2 = int(input('Enter second number: '))
+#print(num_1 > 0)
+#print(num_2 > 0)
+
+
+#user_email = 'Test@gmail.com'
+#email = (input('Enter your email: '))
+#print(user_email == email)
+#user_password = 12345678
+#password = int(input('Enter your password: '))
+#print(user_password == password)
+
+#3) print(12==13 or 12==12 and 21>20) 1. = 
+
+adult_age = 18
+user_age = int(input('Enter Your age: '))
+print(adult_age < user_age)
+
+admin = 'admin'
+user_role = input('Enter your role: ')
+print(admin == user_role)
+
+
+#ლოგიკური ოპერატორები არის: not,or,and
+#შედარების ოპერატორები არის: >,<,==,!>,!<,<=,>=
