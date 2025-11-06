a899fa8927d203ea38be9b2a3ae1415ba1d7aeb7
Niko-glitch777 <nikatsomaia88@gmail.com>
2025-11-06 18:26:13 +0400

    “commit”

---
+++ day14/classwork/cw.py
@@ -0,0 +1,16 @@
+#sequencing - კოდის თანმიმდევრობა
+#iteration - კოდის გამეორება რამოდენიმეჯერ
+#selection - კოდის გადაწყვეტილება
+
+#საიტერაციო ცვლადი - კოდის გამეორება იმდენჯერ რა რიცხვიც წერია
+#range - ში
+
+for i in range(1,20,2):
+    print(i)
+
+
+for i in range(0,50,2):
+    print(i)
+
+for i in range(15):
+    print('Niko')
