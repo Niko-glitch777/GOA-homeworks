dff64991c5bb73f806cad8734a0f6cdd52acb546
Niko-glitch777 <nikatsomaia88@gmail.com>
2025-11-14 12:30:58 +0400

    “commit”

---
+++ day16/classwork/cw.py
@@ -0,0 +1,8 @@
+password = 'Niko123'
+
+
+user_input = input("Enter your password here: ")
+
+
+while password == user_input:
+   print(password == user_input)
---
+++ day16/homework/hw.py
@@ -0,0 +1,24 @@
+
+#number = 1
+#while number <= 10:
+#    print(number)
+#number += 1
+
+
+
+
+
+#number2 = 10
+#while number2 >= 1:
+#    print(number2)
+#number2 -=  1
+
+
+name2 = "Niko"
+
+
+name1 = input("Enter your name: ")
+
+while name1 != name2:
+    name1 = input("Enter your name again: ")
+print("Correct")
