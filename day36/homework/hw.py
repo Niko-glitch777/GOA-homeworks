ae4a01bcdf873600c4c0574fbef354c73dfe0738
Niko-glitch777 <nikatsomaia88@gmail.com>
2026-01-04 16:36:57 +0400

    “commit”

---
+++ day36/homework/hw.py
@@ -0,0 +1,165 @@
+# Grashopper - Basic Function Fixer
+
+# def add_five(num):
+#     total = num + 5
+#     return total
+
+
+# Basic variable assignment
+
+# a = "code"
+# b = "wa.rs"
+# name = a + b
+
+# Grasshopper - Messi goals function
+
+# def goals(laLiga, copaDelRey, championsLeague):
+#     return laLiga + copaDelRey + championsLeague
+
+
+
+# You only need one - Beginner
+
+# def check(seq, elem):
+#     if elem in seq:
+#         return True
+#     else:
+#         return False
+
+# Is he gonna survive?
+
+# def hero(bullets, dragons):
+#     if bullets / 2 >= dragons:
+#         return True
+#     else:
+#         return False
+
+# Is it even?
+
+# def is_even(n): 
+#     if n % 2 == 0:
+#         return True
+#     else:
+#         return False
+
+# Convert a Boolean to a String
+
+# def boolean_to_string(b):
+#     if b == True:
+#         return "True"
+#     else:
+#         return "False"
+
+# Multiply
+
+# def multiply(a, b):
+#     return a * b
+
+# String repeat
+
+# def repeat_str(repeat, string):
+#     return repeat*string
+
+
+# Switch it Up!
+
+# def switch_it_up(number):
+#     if number == 0:
+#         result = "Zero"
+#     elif number == 1:
+#         result = "One"
+#     elif number == 2:
+#         result = "Two"
+#     elif number == 3:
+#         result = "Three"
+#     elif number == 4:
+#         result = "Four"
+#     elif number == 5:
+#         result = "Five"
+#     elif number == 6:
+#         result = "Six"
+#     elif number == 7:
+#         result = "Seven"
+#     elif number == 8:
+#         result = "Eight"
+#     elif number == 9:
+#         result = "Nine"
+#     return result
+
+
+# Grasshopper - Personalized Message
+
+# def greet(name, owner):
+#     if name == owner:
+#         return "Hello boss"
+#     else:
+#         return "Hello guest"
+
+
+# altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+
+# def to_alternating_case(string):
+#     return string.swapcase()
+
+
+# Array plus array
+
+# def array_plus_array(arr1,arr2):
+#     sum = 0
+#     for i in arr1:
+#         sum = sum + i
+#     for i in arr2:
+#         sum = sum + i
+#     return sum
+    
+
+
+# Sum of positive
+
+# def positive_sum(arr):
+#     sum = 0
+#     for i in arr:
+#         if i > 0:
+#             sum=sum+i
+#     return sum
+
+
+# Swap Values
+
+# def swap_values(args): 
+#     args.reverse()
+
+
+# Simple multiplication
+
+# def simple_multiplication(number) :
+#     if number % 2 == 0:
+#         return number*8
+#     else:
+#         return number*9
+
+
+# MakeUpperCase
+
+# def make_upper_case(s):
+#     return s.upper()
+
+
+# Function 1 - hello world
+
+# # Write a function `greet` that returns "hello world!"
+# def greet():
+#     return"hello world!"
+
+# Find the smallest integer in the array
+
+# def find_smallest_int(arr):
+#     return min(arr)
+
+# Convert boolean values to strings 'Yes' or 'No'.
+
+# def bool_to_word(boolean):
+#     if boolean == True:
+#         return "Yes"
+#     else:
+#         return "No"
