46594122af160df56682fe8a1f8c8a690f545daf
Niko-glitch777 <nikatsomaia88@gmail.com>
2026-01-13 19:25:56 +0400

    commit

---
+++ day01/classwork/cw.py
@@ -0,0 +1,24 @@
+
+name = "Niko"
+
+surname = "Tsomaia"
+
+print(name)
+print(surname)
+
+
+# +
+print(6 + 2)
+print(3 + 7)
+
+# *
+print(6 * 5)
+print(5 * 3)
+
+# -
+print(15 - 8)
+print(16 - 3)
+
+# / 
+print(25 / 1)
+print(8 / 2)
--- day02/homework/hw.py
+++ day02/homework/hw.py
@@ -13,6 +13,11 @@ print(number)
 movie = ("dumb and dumber")    # <-- string
 print(movie)
 
+num = 3.5     # <--float
+print(num)
+
+compare = 3      #<-- boolean
+print(compare > 1)
 
 me=(12)
 mom=(30)
