ebf6804777256bf6ce02857d2d3ae30c28fe5fdd
Niko-glitch777 <nikatsomaia88@gmail.com>
2026-01-25 15:35:36 +0400

    “commit”

---
+++ day44/homework/hw.py
@@ -0,0 +1,51 @@
+# Odd or Even?
+# def odd_or_even(arr):
+#     sum = 0 
+#     for i in arr:
+#         sum = sum + i
+#     if sum % 2 == 0:
+#         return "even"
+#     else:
+#         return "odd"
+
+
+# Array plus array
+# def array_plus_array(arr1,arr2):
+#     sum1 = 0
+#     sum2 = 0
+#     sum3 = 0
+#     for i in arr1:
+#         sum1 = sum1 + i
+#     for i in arr2:
+#         sum2 = sum2 + i
+#     sum3 = sum1 + sum2
+#     return sum3
+
+
+#Invert Values
+# def invert(lst):
+#     l = []
+#     for i in lst:
+#         l.append(i*(-1))
+#     return l
+
+#Simple multiplication
+# def simple_multiplication(number) :
+#     if number % 2 == 0:
+#         return number*8
+#     else:
+#         return number*9
+    
+
+# Remove String Spaces
+# def no_space(x):
+#     sting = ""
+#     for i in x:
+#         if not i == " ":
+#             sting = sting + i
+#     return sting
+
+
+# String repeat
+# def repeat_str(repeat, string):
+#     return repeat*string
