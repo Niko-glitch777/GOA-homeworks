291767d8c2a125926e74d2cb5543f25e450e4171
Niko-glitch777 <nikatsomaia88@gmail.com>
2026-02-04 13:15:11 +0400

    “commit”

---
+++ day48/classwork/cw.py
@@ -0,0 +1,39 @@
+# def invert(lst):
+#     l = []
+#     for i in lst:
+#         l.append(i*(-1))
+#     return l
+
+
+# def array_diff(a, b):
+#     result = []
+#     for i in a:
+#         if i not in b:
+#             result.append(i)
+#     return result
+
+
+# def is_leap_year(year):
+#     if year % 400 == 0:
+#         return True
+#     elif year %100 == 0:
+#         return False
+#     elif year % 4 == 0:
+#         return True
+#     else:
+#         return False
+
+
+# def between(a,b):
+#     sum = []
+#     for i in range(a,b+1):
+#         sum.append(i)
+#     return sum
+
+
+# def double_char(s):
+#     sting = ""
+#     for i in s:
+#         sting+=i*2
+#     return sting
+
---
+++ day48/homework/hw.py
@@ -0,0 +1,34 @@
+#5 Without numbers!!!
+# def unusual_five():
+#     return len("wordd")
+
+# Reversed sequence
+# def reverse_seq(n):
+#     l = []
+#     for i in range(n,0,-1):
+#         l.append(i)
+#     return l
+
+
+# Array.diff
+# def array_diff(a, b):
+#     result = []
+#     for i in a:
+#         if i not in b:
+#             result.append(i)
+#     return result
+
+# Remove String Spaces
+# def no_space(x):
+#     sting = ""
+#     for i in x:
+#         if not i == " ":
+#             sting = sting + i
+#     return sting
+
+
+# def reverse_words(s):
+#     l = s.split(" ")
+#     l.reverse()
+#     return " ".join(l) 
+
